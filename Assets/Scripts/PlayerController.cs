450c2c6bc339fb809f3802501fac950e7753aa06
Tom Slavin (RIT Student) <tps3871@ad.rit.edu>
2020-02-21 20:01:05 -0500

    Created test scene folder, basic character movement and alien path-following logic

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D), typeof(CircleCollider2D))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float walkSpeed = 1.0f;
+    [SerializeField] float accelerationRate = 0.5f;
+
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 movementInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        if (movementInput.sqrMagnitude > 0) Move(movementInput);
+    }
+
+    void Move(Vector2 input)
+    {
+        float impulse = rb.mass * (walkSpeed * accelerationRate) * Time.deltaTime;
+        rb.AddForce(input.normalized * impulse, ForceMode2D.Impulse);
+        LimitSpeed();
+    }
+
+    // Limits RB velocity to walkspeed
+    void LimitSpeed()
+    {
+        float speed = rb.velocity.magnitude;
+        if (speed > walkSpeed)
+        {
+            // Applies an impulse to slow the object down
+            Vector2 counterForce = -rb.velocity.normalized;
+
+            counterForce *= (rb.mass * (speed - walkSpeed));
+            rb.AddForce(counterForce, ForceMode2D.Impulse);
+        }
+    }
+}
---
+++ Assets/Scripts/WalkingObjective.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D), typeof(CircleCollider2D))]
+public class WalkingObjective : MonoBehaviour
+{
+    [Header("Path-following Properties")]
+    [SerializeField, Tooltip("The maximum radius at which the character will begin seeking the next point")]
+    float goalThreshold = 1.0f;
+
+    [SerializeField]
+    float walkSpeed = 2.0f;
+
+    Queue<Vector2> path;
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        path = new Queue<Vector2>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FollowPath();
+    }
+
+    void FollowPath()
+    {
+        if (path.Count > 0)
+        {
+            Seek(path.Peek());
+
+            if ((path.Peek() - (Vector2)transform.position).sqrMagnitude <= (goalThreshold * goalThreshold))
+            {
+                path.Dequeue();
+            }
+        }
+    }
+
+    void Seek(Vector2 goal)
+    {
+        Vector2 delta = goal - (Vector2)transform.position;
+        Vector2 direction = delta - rb.velocity;
+        rb.AddForce(direction * walkSpeed, ForceMode2D.Impulse);
+
+        LimitSpeed();
+    }
+
+    // Limits RB velocity to walkspeed
+    void LimitSpeed()
+    {
+        float speed = rb.velocity.magnitude;
+        if (speed > walkSpeed)
+        {
+            // Applies an impulse to slow the object down
+            Vector2 counterForce = -rb.velocity.normalized;
+
+            counterForce *= (rb.mass * (speed - walkSpeed));
+            rb.AddForce(counterForce, ForceMode2D.Impulse);
+        }
+    }
+
+    void UpdatePath(List<Transform> points)
+    {
+        foreach (var point in points)
+        {
+            path.Enqueue(point.position);
+        }
+    }
+}
