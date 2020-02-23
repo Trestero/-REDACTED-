784b1e024717588e60391f42cc83f7bcca079b56
tafellappen <msa4295@rit.edu>
2020-02-22 22:24:30 -0500

    UNTESTED spawn civilian code in game manager, player scaring civilians via petition and megaphone code

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,17 +4,65 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    //Person[] people;
+    public Person[] People { get; set; }
+    
+    //quick n dirty put this on corner outside the camera's view and use position for where to spawn people
+    [SerializeField] GameObject ExtentsMarker;
+    [SerializeField] GameObject PersonPrefab;
 
+    //[SerializeField] GameObject yExtentsMarker;
+
+    float xExtents;
+    float yExtents;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        xExtents = ExtentsMarker.transform.position.x;
+        yExtents = ExtentsMarker.transform.position.y;
+        //float vertExtent = Camera.main.orthographicSize;
+        //float horzExtent = vertExtent * Screen.width / Screen.height;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void SpawnPerson()
+    {
+        float ySpawn = yExtents;
+        float xSpawn = xExtents;
+        
+        if(Random.value > 0.5f) //sides
+        {
+            ySpawn = Random.Range(yExtents, -yExtents);
+
+            if (Random.value > 0.5f) //left
+            {
+                xSpawn = -xExtents;
+            }
+            else if(Random.value < 0.5f) //right
+            {
+                xSpawn = xExtents;
+            }
+        }
+        else if(Random.value < 0.5f) //top or bottom
+        {
+            xSpawn = Random.Range(xExtents, -xExtents);
+
+            if (Random.value > 0.5f) //left
+            {
+                ySpawn = -yExtents;
+            }
+            else if (Random.value < 0.5f) //right
+            {
+                ySpawn = yExtents;
+            }
+        }
+
+        Instantiate(PersonPrefab, new Vector2(xSpawn, ySpawn), Quaternion.identity);
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 [RequireComponent(typeof(Rigidbody2D), typeof(CircleCollider2D))]
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float walkSpeed = 1.0f;
     [SerializeField] float accelerationRate = 0.5f;
+    [SerializeField] float yellRange = 1f;
+    [SerializeField] float megaphoneRange = 2f;
+    public Person[] people { get; set; }
+
+    enum CurrentTool
+    {
+        Petition,
+        Megaphone
+    }
+
+    private CurrentTool currentTool;
 
     private Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        currentTool = CurrentTool.Petition;
     }
 
     // Update is called once per frame
@@ -21,6 +34,23 @@ public class PlayerController : MonoBehaviour
         Vector2 movementInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
 
         if (movementInput.sqrMagnitude > 0) Move(movementInput);
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            switch (currentTool)
+            {
+                case CurrentTool.Petition:
+                    Petition();
+                    break;
+
+                case CurrentTool.Megaphone:
+                    Megaphone();
+                    break;
+
+                default:
+                    break;
+            }
+        }
     }
 
     void Move(Vector2 input)
@@ -42,5 +72,38 @@ public class PlayerController : MonoBehaviour
             counterForce *= (rb.mass * (speed - walkSpeed));
             rb.AddForce(counterForce, ForceMode2D.Impulse);
         }
+    }
+
+    void Megaphone()
+    {
+        //get people withn megaphone range
+        Person[] closeEnoughPeople = FindPeopleWithinRange(megaphoneRange);
+        //scare them
+        foreach (Person p in closeEnoughPeople)
+            p.Scare();
+        
+    }
+
+    void Petition()
+    {
+        //get people withn megaphone range
+        Person[] closeEnoughPeople = FindPeopleWithinRange(yellRange);
+        //scare them
+        foreach (Person p in closeEnoughPeople)
+            p.Scare();
+
+    }
+
+    /// <summary>
+    /// returns an array of people within the given range
+    /// </summary>
+    /// <param name="range"></param>
+    /// <returns></returns>
+    Person[] FindPeopleWithinRange(float range)
+    {
+        var closeEnoughPeople = (from p in people
+                                where Vector2.Distance(p.transform.position, transform.position) <= megaphoneRange
+                                select p).ToArray();
+        return closeEnoughPeople;
     }
 }
